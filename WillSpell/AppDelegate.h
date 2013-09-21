91090d3c8003783b31d2be4327f83be03695b8fc
Andy Beck <andybeck@Andys-MacBook-Air.local>
2013-09-20 21:47:24 -0400

    Initial Commit

---
+++ WillSpell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WillSpell
+//
+//  Created by Andy Beck on 9/20/13.
+//  Copyright (c) 2013 Andy Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WillSpell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WillSpell
+//
+//  Created by Andy Beck on 9/20/13.
+//  Copyright (c) 2013 Andy Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
